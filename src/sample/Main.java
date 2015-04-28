800e858237b8147e2fb0dc3d4006ca3e88b05454
dartl <twisan@mail.ru>
2015-04-28 21:48:17 +0300

    Удаление классов и функций связанных с хранением данных в xml

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -5,16 +5,9 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import javax.xml.bind.JAXBContext;
-import javax.xml.bind.Marshaller;
-import javax.xml.bind.Unmarshaller;
-import java.io.File;
-import java.util.HashMap;
 
 public class Main extends Application {
 
-    private File ComicsesFilePath; /* Хранит текущий путь до файла с данными о комиксах */
-    private HashMap<Integer,Comics> Comicses = new HashMap<Integer,Comics>(); /* Хэш таблица с данными о комиксах */
 
     @Override
     public void start(Stage primaryStage) throws Exception{
@@ -22,79 +15,10 @@ public class Main extends Application {
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
-
-        /* тестовый код для проверки сохранения данных в xml формате */
-        ComicsesFilePath = new File("K:/","test.xml");
-        Comics Temp = new Comics("a","b","c",2015,"d","e","f","g");
-        for (int i = 0; i < 5; i++) {
-            Comicses.put(i,Temp);
-        }
-        saveComicsesDataToFile(ComicsesFilePath);
     }
 
 
     public static void main(String[] args) {
         launch(args);
-    }
-
-    /**
-     * Метод для загрузки данных о комиксах из xml формата
-     * @param file Путь до файла из которого происходит загрузка данных
-     */
-    public void loadComicsesDataFromFile(File file) {
-        try {
-            JAXBContext context = JAXBContext
-                    .newInstance(ComicsWrapper.class);
-            Unmarshaller um = context.createUnmarshaller();
-
-            // Читаем XML формат и демаршализируем его
-            ComicsWrapper wrapper = (ComicsWrapper) um.unmarshal(file);
-
-            Comicses.clear();
-            setComicses(wrapper.getComicsMap());
-
-            // Сохраняем текущий путь до файла с данными
-            setComicsesFilePath(file);
-
-        } catch (Exception e) {
-        }
-    }
-
-    /**
-     * Метод сохранения данных о всех комиксах в формате XML
-     * @param file Путь до файла куда сохраняем данные
-     */
-    public void saveComicsesDataToFile(File file) {
-        try {
-            JAXBContext context = JAXBContext
-                    .newInstance(ComicsWrapper.class);
-            Marshaller m = context.createMarshaller();
-            m.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
-
-            ComicsWrapper wrapper = new ComicsWrapper();
-            wrapper.setComicsMap(Comicses);
-
-            m.marshal(wrapper, file);
-
-            setComicsesFilePath(file);
-        } catch (Exception e) {
-        }
-    }
-
-
-    public File getComicsesFilePath() {
-        return ComicsesFilePath;
-    }
-
-    public void setComicsesFilePath(File comicsesFilePath) {
-        ComicsesFilePath = comicsesFilePath;
-    }
-
-    public HashMap<Integer, Comics> getComicses() {
-        return Comicses;
-    }
-
-    public void setComicses(HashMap<Integer, Comics> comicsMap) {
-        Comicses = comicsMap;
     }
 }
